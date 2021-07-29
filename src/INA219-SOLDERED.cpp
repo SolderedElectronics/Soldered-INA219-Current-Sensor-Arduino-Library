a63688cfb534267c660a51e25bb01fa3e175b54d
Main Push Robot <robot@example.com>
2021-07-29 07:30:02 +0000

    Committing dev changes

---
+++ src/INA219-SOLDERED.cpp
@@ -0,0 +1,13 @@
+/**
+ **************************************************
+ *
+ * @file        INA219-SOLDERED.cpp
+ * @brief       Intentionally blank.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Zvonimir Haramustek for soldered.com
+ ***************************************************/
+
+
+// #include "INA219-SOLDERED.h"
---
+++ src/INA219-SOLDERED.h
@@ -0,0 +1,20 @@
+/**
+ **************************************************
+ *
+ * @file        INA219-SOLDERED.h
+ * @brief       INA219 driver implementation header file.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Zvonimir Haramustek for soldered.com
+ ***************************************************/
+
+#ifndef __INA219_SOLDERED__
+#define __INA219_SOLDERED__
+
+#include "Arduino.h"
+#include "Wire.h"
+#include "libs/Arduino-INA219/INA219.h"
+
+
+#endif
