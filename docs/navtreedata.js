fc602f5034f09e35f64e01b259d4fb7cd6488a7b
Docs Robot <robot@example.com>
2023-02-14 15:41:56 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "INA219 Arduino Library by Soldered", "index.html", [
-    [ "Voltage & current sensor INA219 breakout Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "Voltage & current sensor INA219 breakout Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
